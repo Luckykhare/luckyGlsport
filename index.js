f751d68e3abd8749faf174b399480b06295cd9e6
Luckykhare <118264095+Luckykhare@users.noreply.github.com>
2022-11-15 12:03:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/*javascript variable
+what are variable?-containe to store data values*/
+var number1=34;
+var number2=58;
+console.log(number1+number2);
+
+//Datatype injavasript
+//Numbers
+var num1=34;
+var num2=56;
+
+
+//string
+var str1 =" this is atring";
+var str2 ="this is string 2";
+
+//objects
+var marks ={
+    ravi:76,
+    harry:99.92,
+    shubam:78,
+
+
+}
